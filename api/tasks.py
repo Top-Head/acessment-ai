854490d4d8eae8ab2421d51922b56af531a20283
Omar Rodrigues <omarscode007@gmail.com>
2026-05-19 22:40:07 +0100

    refactor: NOw using docker file to run the project

--- api/tasks.py
+++ api/tasks.py
@@ -1,4 +1,3 @@
-# api/tasks.py
 from celery import shared_task
 from api.models import Student, Key, StudentAnswer
 from api.services.cloduinary import CloudinaryConfig
--- api/views/chave_views.py
+++ api/views/chave_views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 import fitz
 
 from api.models import Key
